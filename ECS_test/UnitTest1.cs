2fb89cf949487a46291660f75fdaacfe28f01bd3
codeSoren <81234789+JAY-Z-Sharp@users.noreply.github.com>
2022-02-17 12:48:32 +0100

    Test setup

--- ECS_test/UnitTest1.cs
+++ ECS_test/UnitTest1.cs
@@ -1,12 +1,17 @@
 using NUnit.Framework;
 
+
 namespace ECS_test
 {
     public class Tests
     {
+
+        private ECS.Legacy_Refactored.ECS testECS;
+
         [SetUp]
         public void Setup()
         {
+            testECS = new ECS.Legacy_Refactored.ECS(23, new FakeTempSensor(), new FakeHeater());
         }
 
         [Test]
